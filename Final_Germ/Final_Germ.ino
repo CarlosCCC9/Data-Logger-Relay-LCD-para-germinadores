0cce92ae6d6fbaff68ce9602ab06b6eacdfac5a9
CarlosCCC9 <71854944+CarlosCCC9@users.noreply.github.com>
2022-11-15 11:15:58 -0400

    Actualizacion
    Codigo final

--- Final_Germ/Final_Germ.ino
+++ Final_Germ/Final_Germ.ino
@@ -1,82 +1,82 @@
-//Librerias necesarias
-#include <SPI.h>      
-#include <SD.h>       
+//Librerias
+#include <SPI.h>
+#include <SD.h>
 #include <DHT.h>
+#include <Wire.h>
+#include <ds3231.h>
 #include <LiquidCrystal.h>
-#include <DS3231.h>
 
 //Sensores
 #define DHTPIN1 8
 #define DHTPIN2 9
-#define DHTTYPE DHT11 
+#define DHTTYPE DHT22 
 DHT dht1(DHTPIN1, DHTTYPE);
 DHT dht2(DHTPIN2, DHTTYPE);
-/*
-DHT dht = {
-{DHTPIN1, DHT22},
-{DHTPIN2, DHT22},
-};*/
 float humidity[2];
 float temperature[2];
 
-//parametros del reloj
-DS3231  rtc(SDA, SCL);
-Time t;
+//SD
+File dataFile;
 
-const int OnHour = 22; //hora para encender
-const int OnMin = 48;
-const int OffHour = 22; //hora para apagar
-const int OffMin = 49;
-
-//SD Logger
-File datafile;
-
-//Pantalla
-LiquidCrystal lcd(7, 6, 5, 4, 3, 2);	// pines RS, E, D4, D5, D6, D7
+//Reloj
+struct ts t;
 
 //Relays
 int Relaypin1= 16;
 int Relaypin2= 17;
+const int OnHour = 12; //hora para encender
+const int OnMin = 10;
+const int OffHour = 12; //hora para apagar
+const int OffMin = 40;
 
-void setup() {
+//LCD
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+
+void setup(){
   Serial.begin(9600);
-  //for (auto& sensor : dht) {sensor.begin();}
+  while (!Serial)
+    ; // wait for serial port to connect. Needed for native USB port only
+  Serial.print("Initializing SD card...");
+  if (!SD.begin()) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
   lcd.begin(16, 2);
+  dht1.begin();
+  dht2.begin();
+  Wire.begin();
+  DS3231_init(DS3231_CONTROL_INTCN);
   pinMode(Relaypin1, OUTPUT);
   pinMode(Relaypin2, OUTPUT);
   digitalWrite(Relaypin1, HIGH);
   digitalWrite(Relaypin2, HIGH);
-
+  /*----------------------------------------------------------------------------
+  Quitar los comentarios pra fijar nueva fecha y hora
+  ----------------------------------------------------------------------------*/
+  //t.hour=12; 
+  //t.min=30;
+  //t.sec=0;
+  //t.mday=25;
+  //t.mon=12;
+  //t.year=2019;
+  //DS3231_set(t);
+ 
+  delay(2000);
 }
-
-void loop() {
-  //obtener valores de temperatura y humedad
-  
-  /*for (int i = 0; i < 2; i++) {
-    temperature[i] = dht[i].readTemperature();
-    humidity[i] = dht[i].readHumidity();
-  }*/
+void loop(){
   temperature[0] = dht1.readTemperature();
   humidity[0] = dht1.readHumidity();
   temperature[1] = dht2.readTemperature();
   humidity[1] = dht2.readHumidity();
-  //Obtener tiempo
-  t=rtc.getTime();
-   //abrir sd
-  datafile = SD.open("TempLog.csv", FILE_WRITE);
-  if (datafile) {
-    for (int i = 0; i < 2; i++) {
-      datafile.print(temperature[i]);
-      datafile.print(",");
-      datafile.print(humidity[i]);
-      datafile.print(",");
-      }
-    datafile.print(t.hour);
-    datafile.print(",");
-    datafile.println(t.min);
-    datafile.close();
+  for (int i = 0; i < 2; i++){
+    Serial.print(temperature[i]);
+    Serial.print("\t");
+    Serial.print(humidity[i]);
+    Serial.print("\t");
   }
-  //pantalla
+  Serial.print("\n");
+ //pantalla
   lcd.setCursor(0, 0);
   lcd.print("T1");
   lcd.print(temperature[0]);
@@ -87,16 +87,39 @@ void loop() {
   lcd.print(temperature[1]);
   lcd.print("H2");
   lcd.print(humidity[1]);
-  //Relays
-  if(t.hour == OnHour && t.min == OnMin){
+  
+  dataFile = SD.open("TempLog.txt", FILE_WRITE);
+  if (dataFile) {
+    for (int i = 0; i < 2; i++){
+      dataFile.print(temperature[i]);
+      dataFile.print(",");
+      dataFile.print(humidity[i]);
+      dataFile.print(",");
+    }
+    DS3231_get(&t);
+    dataFile.print(t.hour);
+    dataFile.print(",");
+    dataFile.print(t.min);
+    dataFile.print("\n");
+    dataFile.close();
+    lcd.print("S");
+   }
+  // Si no hay SD
+  else{
+    Serial.println("error opening DHT11Log.txt");
+    lcd.print("N");
+  }
+ DS3231_get(&t);
+  //if(t.hour == OnHour && t.sec == OnMin){
+  if (t.sec==10){
     digitalWrite(Relaypin1,LOW);
     digitalWrite(Relaypin2,LOW);
   }
-  else if(t.hour == OffHour && t.min == OffMin){
+  //else if(t.hour == OffHour && t.sec == OffMin){
+  else if(t.sec==40){
     digitalWrite(Relaypin1,HIGH);
     digitalWrite(Relaypin2,HIGH);
     }
 
-
-delay(10000);
+  delay(1000);
 }
